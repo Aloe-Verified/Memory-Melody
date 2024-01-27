3fd92f4662551d0afd6fcc719314b1e347bcb402
Benjamin Wang <benjaminwang42@gmail.com>
2024-01-27 13:32:24 -0500

    Create login page file

---
+++ nostalgia_webapp/src/Pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../Styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home" >
+      <div className="headerContainer" >
+        <h1> Hello</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
