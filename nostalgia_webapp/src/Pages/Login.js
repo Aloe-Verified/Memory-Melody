2f9536461a812dad1b7e5830f2b84d22a2f82707
Benjamin Wang <benjaminwang42@gmail.com>
2024-01-27 22:50:20 -0500

    Style home page

--- nostalgia_webapp/src/Pages/Login.js
+++ nostalgia_webapp/src/Pages/Login.js
@@ -10,10 +10,8 @@ function Login() {
       <div className="container">
           <div className="banner">
               <div className="banner-content">
-                  <h5>WELCOME TO</h5>
                   <h2>Memory <span>Melody</span></h2>
-                  <p>
-Embark on a delightful journey as you seamlessly explore cherished images intertwined with the familiar melodies that resonate with your memories!</p>
+                  <p>Embark on a delightful journey as you seamlessly explore cherished images intertwined with the familiar melodies that resonate with your memories!</p>
               </div>
               <div className="banner-img">
                 <img src={img} alt="Logo picture"></img>
