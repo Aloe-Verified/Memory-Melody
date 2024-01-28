1c20972a1ede7193a2adb04b42afacd333d28299
Travis Pan <trakabeth@gmail.com>
2024-01-28 04:26:15 -0500

    Style background for home page

--- nostalgia_webapp/src/Pages/Home.js
+++ nostalgia_webapp/src/Pages/Home.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import "../Styles/Home.css";
-<<<<<<< HEAD
-=======
 import background from "../Assets/HomeBackground.jpg";
->>>>>>> c66f87f8a5a967772d81006743d772225fdada27
 
 function Home() {
   return (
